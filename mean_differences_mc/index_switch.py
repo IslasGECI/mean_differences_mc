476e175f7370137befd02b0785a1c79a91962807
Nepo Rojas <braulio.rojas@islas.org.mx>
2020-03-12 13:47:44 -0700

    Modifica función para que utilice límite superior

--- mean_differences_mc/index_switch.py
+++ mean_differences_mc/index_switch.py
@@ -1,5 +1,5 @@
 from random import randint
 
 def index_to_switch(lim_max : int):
-    index = [randint(0, 3), randint(0, 3)]
+    index = [randint(0, lim_max), randint(0, lim_max)]
     return(index)
