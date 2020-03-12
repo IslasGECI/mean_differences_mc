eaeb6c381dc269cb7726c4c0a0c71d225a3be491
Nepo Rojas <braulio.rojas@islas.org.mx>
2020-03-12 11:42:42 -0700

    Modifica la función para que pase la segunda prueba

--- mean_differences_mc/switch_elements.py
+++ mean_differences_mc/switch_elements.py
@@ -1,4 +1,6 @@
 def switch_elements(A : list, B : list, index : list):
-    A_final: list = [3, 7, 3, 3]
-    B_final: list = [7, 7, 7, 3]
+    A_final: list = A.copy()
+    A_final[index[0]] = B[index[1]]
+    B_final: list = B.copy()
+    B_final[index[1]] = A[index[0]]
     return(A_final, B_final)
