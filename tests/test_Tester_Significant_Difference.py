7b7ca72c629cc12b15fb7cd32e94e403b363bf2f
Nepo Rojas <braulio.rojas@islas.org.mx>
2020-03-13 14:11:26 -0700

    Prueba que propiedad `sample_b` funciona bien

--- tests/test_Tester_Significant_Difference.py
+++ tests/test_Tester_Significant_Difference.py
@@ -11,9 +11,9 @@ class TestTesterSignificantDifference(unittest.TestCase):
         Crea variables que se usarán en las pruebas
         """
         self.sample_a : pd.DataFrame = pd.DataFrame({'sample_a' : [3, 3, 3, 3]})
-        self.sampla_a_array : np.array = np.array(self.sample_a['sample_a'])
+        self.sample_a_array : np.array = np.array(self.sample_a['sample_a'])
         self.sample_b : pd.DataFrame = pd.DataFrame({'sample_b' : [7, 7, 7, 7]})
-        self.sampla_b_array : np.array = np.array(self.sample_b['sample_b'])
+        self.sample_b_array : np.array = np.array(self.sample_b['sample_b'])
         self.Probador_Diferencias_Significativas : Tester_Significant_Difference = Tester_Significant_Difference()
 
     def test_initialization(self):
@@ -28,10 +28,13 @@ class TestTesterSignificantDifference(unittest.TestCase):
         """
         self.Probador_Diferencias_Significativas.sample_a = self.sample_a
         muestra_a : np.array = self.Probador_Diferencias_Significativas.sample_a
-        self.assertTrue((self.sampla_a_array == muestra_a).all())
+        self.assertTrue((self.sample_a_array == muestra_a).all())
         self.Probador_Diferencias_Significativas.sample_a = self.sample_b
         muestra_a : np.array = self.Probador_Diferencias_Significativas.sample_a
-        self.assertTrue((self.sampla_b_array == muestra_a).all())
+        self.assertTrue((self.sample_b_array == muestra_a).all())
+        self.Probador_Diferencias_Significativas.sample_b = self.sample_b
+        muestra_b : np.array = self.Probador_Diferencias_Significativas.sample_b
+        self.assertTrue((self.sample_b_array == muestra_b).all())
 
 if __name__ == '__main__':
     unittest.main()
