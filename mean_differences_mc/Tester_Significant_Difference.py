137ae45e1756f91ec9211ee81409516220ca1ca5
Nepo Rojas <braulio.rojas@islas.org.mx>
2020-03-13 14:39:32 -0700

    Agrega la propiedad `sample_b`

--- mean_differences_mc/Tester_Significant_Difference.py
+++ mean_differences_mc/Tester_Significant_Difference.py
@@ -14,3 +14,12 @@ class Tester_Significant_Difference:
     def sample_a(self, sample_a : pd.DataFrame):
         name_column = sample_a.columns[0]
         self.__sample_a = np.array(sample_a[name_column])
+
+    @property
+    def sample_b(self):
+        return(self.__sample_b)
+
+    @sample_b.setter
+    def sample_b(self, sample_b : pd.DataFrame):
+        name_column = sample_b.columns[0]
+        self.__sample_b = np.array(sample_b[name_column])
