4fa6aeb331844c0bc45ff9c38acc22e31c733a2f
Nepo <braulio.rojas@islas.org.mx>
2020-03-23 13:38:28 -0700

    Dale formato con `black`

--- mean_differences_mc/Tester_Significant_Difference.py
+++ mean_differences_mc/Tester_Significant_Difference.py
@@ -1,25 +1,26 @@
 import pandas as pd
 import numpy as np
 
+
 class Tester_Significant_Difference:
     def __init__(self):
-        self._sample_a : np.array = np.array([])
-        self._sample_b : np.array = np.array([])
+        self._sample_a: np.array = np.array([])
+        self._sample_b: np.array = np.array([])
 
     @property
     def sample_a(self):
-        return(self.__sample_a)
+        return self.__sample_a
 
     @sample_a.setter
-    def sample_a(self, sample_a : pd.DataFrame):
+    def sample_a(self, sample_a: pd.DataFrame):
         name_column = sample_a.columns[0]
         self.__sample_a = np.append(self._sample_a, np.array(sample_a[name_column]))
 
     @property
     def sample_b(self):
-        return(self.__sample_b)
+        return self.__sample_b
 
     @sample_b.setter
-    def sample_b(self, sample_b : pd.DataFrame):
+    def sample_b(self, sample_b: pd.DataFrame):
         name_column = sample_b.columns[0]
         self.__sample_b = np.append(self._sample_b, np.array(sample_b[name_column]))
