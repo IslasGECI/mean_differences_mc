585f02abec38be82525b18e8d6193c0c743b2512
Nepo <braulio.rojas@islas.org.mx>
2020-05-04 09:52:04 -0700

    Prueba que la clase permute elementos de las mustras

--- tests/test_Tester_Significant_Difference.py
+++ tests/test_Tester_Significant_Difference.py
@@ -14,6 +14,9 @@ class TestTesterSignificantDifference(unittest.TestCase):
         self.sample_a_array: np.array = np.array(self.sample_a["sample_a"])
         self.sample_b: pd.DataFrame = pd.DataFrame({"sample_b": [7, 7, 7, 7]})
         self.sample_b_array: np.array = np.array(self.sample_b["sample_b"])
+        self.index: list = [1, 3]
+        self.sample_a_final: np.array = np.array([3., 7., 3., 3.])
+        self.sample_b_final: np.array = np.array([7., 7., 7., 3.])
         self.Probador_Diferencias_Significativas: Tester_Significant_Difference = Tester_Significant_Difference()
 
     def test_initialization(self):
@@ -37,6 +40,19 @@ class TestTesterSignificantDifference(unittest.TestCase):
         muestra_b: np.array = self.Probador_Diferencias_Significativas.sample_b
         self.assertTrue((self.sample_b_array == muestra_b).all())
 
+
+    def test_switch_elements(self):
+        """
+        Verifica que los objetos de las clase `Tester_Significant_Difference` permute dos elementos 
+        de las muestras. 
+        """
+        self.Probador_Diferencias_Significativas.sample_a = self.sample_a
+        self.Probador_Diferencias_Significativas.sample_b = self.sample_b
+        self.Probador_Diferencias_Significativas.switch_elements(self.index)
+        muestra_a: np.array = self.Probador_Diferencias_Significativas.sample_a
+        self.assertTrue((self.sample_a_final == muestra_a).all())
+        muestra_b: np.array = self.Probador_Diferencias_Significativas.sample_b
+        self.assertTrue((self.sample_b_final == muestra_b).all())
 
 if __name__ == "__main__":
     unittest.main()
