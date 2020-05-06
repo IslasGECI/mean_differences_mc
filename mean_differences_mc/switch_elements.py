18b8b53cda57a556bfd65a021505a1d041fa3bb2
Fernando <fernando.alvarez@islas.org.mx>
2020-05-06 16:12:14 -0700

    corrige variables de salida

--- mean_differences_mc/switch_elements.py
+++ mean_differences_mc/switch_elements.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 def switch_elements(A : list, B : list, index : list):
     A_final: list = A.copy()
@@ -12,4 +13,6 @@ def switch_elements_arrays(A : np.array, B : np.array, index : list):
     A_final[index[0]] = B[index[1]]
     B_final: np.array = np.copy(B)
     B_final[index[1]] = A[index[0]]
-    return(A_final, B_final)
+    A_final_dataframe = pd.DataFrame(A_final)
+    B_final_dataframe = pd.DataFrame(B_final)
+    return(A_final_dataframe, B_final_dataframe)
