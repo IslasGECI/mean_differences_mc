3dffe7616102349712c2010f54b19d716f48c84a
Nepo <braulio.rojas@islas.org.mx>
2020-08-05 15:57:48 -0700

    Pasa la primer prueba

--- mean_differences_mc/__init__.py
+++ mean_differences_mc/__init__.py
@@ -1,3 +1,4 @@
 from .Tester_Significant_Difference import *
 from .switch_elements import *
 from .index_switch import *
+from .calculate_mean_difference import *
---
+++ mean_differences_mc/calculate_mean_difference.py
@@ -0,0 +1,2 @@
+def calculate_mean_difference(minuend, subtrahend):
+    return -4.0
