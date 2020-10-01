04ea902889dd19c0681c9df08a2dc8e491ecc40e
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2020-10-01 10:59:31 -0700

    ♻️ Refactoriza eliminando else

--- mean_differences_mc/generalized_monte_carlo_test.py
+++ mean_differences_mc/generalized_monte_carlo_test.py
@@ -2,10 +2,10 @@ from mean_differences_mc import *
 
 
 def generalized_monte_carlo_test(sample_a, sample_b, switches=100):
-    if len(sample_a) == len(sample_b):
-        mean_switched_differences = calculate_mean_switched_difference(sample_a, sample_b, switches)
-    else:
+    is_different_length = len(sample_a) != len(sample_b)
+    if is_different_length:
         raise ValueError("sample_a and sample_b must be of the same length")
+    mean_switched_differences = calculate_mean_switched_difference(sample_a, sample_b, switches)
     mean_difference = calculate_mean_difference(sample_a, sample_b)
     p_value = calculate_p_value_from_difference(
         difference=mean_difference, difference_array=mean_switched_differences
