d152ed70938b4f8642a7148766498b0b2dd995ec
Nepo Rojas • GECI <braulio.rojas@islas.org.mx>
2024-11-11 13:20:36 -0800

    🚧 Use pyproject

--- mean_differences_mc/__init__.py
+++ mean_differences_mc/__init__.py
@@ -1,3 +1,6 @@
+"A module to test mean differences mc"
+
+__version__ = "0.1.0"
 from .tester_significant_difference import *  # noqa
 from .switch_elements import *  # noqa
 from .index_switch import *  # noqa
